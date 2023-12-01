2a5bff647813e5a3a6e544176ee57e1b5d6d5731
Colin Bate <colin@colinbate.com>
2023-12-01 16:22:14 -0400

    Initial commit - setup app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Advent of Svelte 2023 Solutions</h1>
+<h2>Colin Bate</h2>
+<ul>
+  <li><a href="/day1">Day 1</a></li>
+</ul>
+<svelte:head><title>Advent of Svelte 2023</title></svelte:head>
---
+++ src/routes/day1/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Day 1</h1>
+<svelte:head><title>Day 1 :: AoS 2023</title></svelte:head>
